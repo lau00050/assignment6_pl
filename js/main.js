df5039eb8ad8bbd7e33d49e65a64bd168f33a342
lau00050 <phil.lauricella@gmail.com>
2018-08-26 22:57:54 +1000

    Version 2
    Got help from Alonso to help finish this one! Images now working yay! :)

--- js/main.js
+++ js/main.js
@@ -6,21 +6,42 @@ cities.forEach(function(cityTitle) {
 	$('#city-type').append('<option>' + cityTitle + '</option>')
 })
 
+// // If a city name is selected, change the background image //
+// $('#city-type').change(function() {
+// 	if (cities[0] === 'Austin') { // ONLY THIS ONE WORKS FRICKEN HELL I'M CONFUSED!!!
+// 		$('body').css('background-image','url(images/austin.jpg)')
+// 		console.log('austin')
+// 	} else if (cities[1] === 'Los Angeles') {
+// 		$('body').css('background-image','url(images/la.jpg)')
+// 		console.log('la')
+// 	} else if (cities[2] === 'New York City')	{
+// 		$('body').css('background-image','url(images/nyc.jpg)')
+// 		console.log('nyc')
+// 	} else if (cities[3] === 'San Francisco') {
+// 		$('body').css('background-image','url(images/sf.jpg)')
+// 		console.log('sf')
+// 	} else if (cities[4] === 'Sydney') {
+// 		$('body').css('background-image','url(images/sydney.jpg)')
+// 		console.log('syd')
+// 	} else {
+// 	 $('body').css('background-image','url(images/citipix_skyline.jpg)')
+// 	}
+// })
+
 // If a city name is selected, change the background image //
 $('#city-type').change(function() {
-	if (cities[0] === 'Austin') { // ONLY THIS ONE WORKS FRICKEN HELL I'M CONFUSED!!!
+	if ($('#city-type').val() === 'Austin') {
 		$('body').css('background-image','url(images/austin.jpg)')
-		console.log('austin')
-	} else if (cities[1] === 'Los Angeles') {
-		$('body').css('background-image','url(images/la.jpg)')
-		console.log('la')
-	} else if (cities[2] === 'New York City')	{
-		$('body').css('background-image','url(images/nyc.jpg)')
-		console.log('nyc')
-	} else if (cities[3] === 'San Francisco') {
+	} else if ($('#city-type').val() === 'Los Angeles') {
+ 		$('body').css('background-image','url(images/la.jpg)')
+ 		console.log('la')
+ 	} else if ($('#city-type').val() === 'New York City')	{
+ 		$('body').css('background-image','url(images/nyc.jpg)')
+ 		console.log('nyc')
+	} else if ($('#city-type').val() === 'San Francisco') {
 		$('body').css('background-image','url(images/sf.jpg)')
 		console.log('sf')
-	} else if (cities[4] === 'Sydney') {
+	} else if ($('#city-type').val() === 'Sydney') {
 		$('body').css('background-image','url(images/sydney.jpg)')
 		console.log('syd')
 	} else {
